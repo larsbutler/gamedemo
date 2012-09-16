eb2ed4ace0a32490ebd3366e43bfba6582494e81
Lars Butler <Lars.Butler@gmail.com>
2012-09-16 02:32:59 +0200

    Flesh out Entity and Player
    Entity now everything it needs for tracking state (including previous state).
    
    Player extends Entity and can render itself.

--- src/com/larsbutler/gamedemo/models/Entity.java
+++ src/com/larsbutler/gamedemo/models/Entity.java
@@ -2,7 +2,94 @@ package com.larsbutler.gamedemo.models;
 
 import java.awt.Graphics;
 
+import com.larsbutler.gamedemo.math.State;
+
 public abstract class Entity {
+
+    protected State xState;
+    protected State yState;
+    protected State prevXState;
+    protected State prevYState;
+
+    protected int width;
+    protected int height;
+
+    public Entity(double x, double y, int width, int height) {
+        xState = new State();
+        xState.p = x;
+        yState = new State();
+        yState.p = y;
+
+        prevXState = new State();
+        prevYState = new State();
+
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getX() {
+        return xState.p;
+    }
+
+    public void setX(double newX) {
+        xState.p = newX;
+    }
+
+    public double getY() {
+        return yState.p;
+    }
+
+    public void setY(double newY) {
+        yState.p = newY;
+    }
+
+    public State getXState() {
+        return xState;
+    }
+
+    public void setXState(State x) {
+        this.xState = x;
+    }
+
+    public State getYState() {
+        return yState;
+    }
+
+    public void setYState(State y) {
+        this.yState = y;
+    }
+
+    public State getPrevXState() {
+        return prevXState;
+    }
+
+    public void setPrevXState(State prevX) {
+        this.prevXState = prevX;
+    }
+
+    public State getPrevYState() {
+        return prevYState;
+    }
+
+    public void setPrevYState(State prevY) {
+        this.prevYState = prevY;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 
     public abstract void render(Graphics g, double alpha);
 }
--- src/com/larsbutler/gamedemo/models/Player.java
+++ src/com/larsbutler/gamedemo/models/Player.java
@@ -1,5 +1,19 @@
 package com.larsbutler.gamedemo.models;
 
-public class Player {
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Player extends Entity {
+
+    public Player(double x, double y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+    public void render(Graphics g, double alpha) {
+        g.setColor(Color.orange);
+        g.fillRect(
+            (int)Math.round(getX()), (int)Math.round(getY()),
+            getWidth(), getHeight());
+    }
 
 }
